b07a8f58a794222b74ea703b3470d2c900526afb
Eugene Song <songeu@os1.engr.oregonstate.edu>
2023-02-01 16:34:24 -0800

    Working on Word Splitting using strtok(). Created 2 different function definitions for split_word(). One has tokenArray, other doesn't - to test the function.

--- smallsh.c
+++ smallsh.c
@@ -3,6 +3,49 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h> 
+
+
+/* Convenient macro to get the length of an array (number of elements) */
+#define arrlen(a) (sizeof(a) / sizeof *(a))
+
+
+static int split_word(char *input, const char *delim, char *tokenArray[]);
+
+
+
+
+
+
+//static int splitword(char *input, const char *delim, char *tokenArray[]) {
+
+static int splitword(char *input, const char *delim) {
+  
+  char *ifs_env = getenv("IFS");
+  char *token = NULL;
+
+  if (ifs_env == NULL) {
+    ifs_env = " \t\n"; 
+  }
+
+  //while (strtok(char *input, ifs_env, tokenArray) !== NULL) {
+ 
+  token = strtok(input, ifs_env); 
+  printf("First token = %s\n", token); 
+  for (;;) {
+    token = strtok(NULL, ifs_env);
+    printf("Next Token = %s\n", token); 
+
+    if (token == NULL) { break;} 
+
+  }
+
+  return 0; 
+}
+
+
+
+
 
 
 
@@ -51,6 +94,8 @@ int main(void) {
 
   return 0; 
 }
+
+
 
 void *str_substitute(char *restrict *restrict haystack, char const *restrict needle, char const *restrict sub) {
   
