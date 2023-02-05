878bdc9d5c4bdb69e823519e0b8300012f6f65d8
Eugene Song <songeu@os1.engr.oregonstate.edu>
2023-02-04 20:02:47 -0800

    I think "managingn background processes" of Input is done. Still need to test.

--- smallsh.c
+++ smallsh.c
@@ -5,8 +5,12 @@
 #include <stdlib.h>
 #include <string.h>
 #include <stddef.h>
-
-
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <errno.h>
+#include <signal.h>
+#include <stdint.h>
 
 /* Convenient macro to get the length of an array (number of elements) */
 #define arrlen(a) (sizeof(a) / sizeof *(a))
@@ -56,7 +60,7 @@ int main(int argc, char *argv[]) {
     if (!ret) exit(1); 
     line2 = ret;
   }
-  printf("%s", line); 
+  printf("%s", line2); 
 
   free(line2); 
 
@@ -122,29 +126,44 @@ int main(int argc, char *argv[]) {
 
 static void perform_expansion(int elements, char *ptrArray[]) {
 
+  char temp[50];
   char *expan_env = NULL;
   expan_env = getenv("HOME");
   printf("\nexpan_env is %s\n", expan_env); 
 
-   int i = 0;
-   printf("size of ptrArray is %d\n", elements);
+  int i = 0;
+  printf("size of ptrArray is %d\n", elements);
 
-   while (i < elements) {
+  while (i < elements) {
 
-     // ensure each token is >= size 2
-     if (strlen(ptrArray[i]) >= 2) {
-          
-              // “~/” at the beginning of a word shall be replaced with the value of the HOME environment variable
-          if (strncmp(ptrArray[i], "~/", 2) == 0) {
-              str_substitute(&ptrArray[i], "~", expan_env);
-          }
+    // ensure each token is >= size 2
+    if (strlen(ptrArray[i]) >= 2) {
+
+      // bullet point 1: “~/” at the beginning of a word shall be replaced with the value of the HOME environment variable
+      if (strncmp(ptrArray[i], "~/", 2) == 0) {
+        str_substitute(&ptrArray[i], "~", expan_env);
+      }
+
+      // pre- bullet point 2: get the smallsh pid and transform into a str to use in str_substitute() 
+      pid_t pid = getpid();
+      if (sprintf(temp, "%d", (int)pid) < 0) {
+        perror("sprintf failed... $$ replacement failed"); 
+      }
+
+      // bullet point 2: occurrence of “$$” within a word shall be replaced with the proccess ID of the smallsh process
+      str_substitute(&ptrArray[i], "$$", temp); 
+
+      // bullet point 3: occurrence of “$?” within a word shall be replaced with the exit status of the last foreground command (see waiting)
 
 
-         }
+      // bullet point 4: occurrence of “$!” within a word shall be replaced with the process ID of the most recent background process (see waiting)
 
-     i++; 
-         }
- 
+
+    }
+
+    i++; 
+  }
+
 }
 
 
@@ -266,3 +285,57 @@ exit:
   return str; 
 }
 
+
+// check for unwaited-for-background processes
+void manage_background_processes() {
+
+  // get process group id of calling process..always sucess 
+  pid_t currGroupId = getpgrp();
+  pid_t childPID = 0;
+  int childStatus = 0;
+
+  // WNOHANG flag - waitpid() returns 0 when no child process terminates...
+  // WUNTRACED constant - for stopped processes
+  //      -1 --> fork() failed
+  //      0 --> child process executes chunk 
+  //      > 0 --> child process PID 
+
+  while ((childPID = waitpid(-1, &childStatus, WNOHANG | WUNTRACED)) > 0) {
+
+    pid_t childPGID = getpgid(childPID);
+
+    if (childPGID == -1) {
+      perror("There was an error calling getpgid()"); 
+    }
+    // check if child process group id of current child process == parent process group id
+    if (getpgid(childPID) == currGroupId) {
+
+      // if exited normally 
+      if (WIFEXITED(childStatus)) {
+        int exitStatus = WEXITSTATUS(childStatus);
+        fprintf(stderr, "Child process %jd done. Exit status %d.\n", (intmax_t) childPID, exitStatus);
+      }
+
+      // if signaled
+      else if (WIFSIGNALED(childStatus)) {
+        int signalNumber = WTERMSIG(childStatus);
+        fprintf(stderr, "Child process %jd done. Signaled %d.\n", (intmax_t) childPID, signalNumber);
+      }
+
+      // if stopped 
+      else if (WIFSTOPPED(childStatus)) {
+        int killStatus = 0;
+
+        // if SIGCONT kill() fails
+        if ((killStatus = kill(childPID, SIGCONT)) == -1) {
+          perror("kill() failed with the childPID"); 
+        }
+        fprintf(stderr, "Child process %jd stopped. Continuing.\n", (intmax_t) childPID); 
+      }
+
+      // other state changes: ignore 
+    }
+  }
+
+
+}
